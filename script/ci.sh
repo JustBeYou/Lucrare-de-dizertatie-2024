22a85f10aa321c23a669e383d03eef2cd92e2fe6
Mihail Feraru <mihailferaru2000@gmail.com>
2024-02-17 17:29:26 +0200

    feature: Load RoBERT and test on RoSent.

--- script/ci.sh
+++ script/ci.sh
@@ -2,6 +2,5 @@
 
 set -e -x
 
-python3 -m black --check dizertatie/ tests/
-python3 -m isort --check --diff dizertatie/ tests/
+python3 -m black --check --diff dizertatie/ tests/
 python3 -m coverage run -m unittest -v
