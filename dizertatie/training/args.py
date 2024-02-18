f588446a2d7ba4926ac11a955362848b843af4ea
Mihail Feraru <mihailferaru2000@gmail.com>
2024-02-18 14:09:01 +0200

    feature: Training for summarization.

--- dizertatie/training/args.py
+++ dizertatie/training/args.py
@@ -2,7 +2,7 @@ import dataclasses
 from pathlib import Path
 from typing import Optional
 
-from transformers import TrainingArguments
+from transformers import Seq2SeqTrainingArguments, TrainingArguments
 
 
 @dataclasses.dataclass
@@ -14,17 +14,28 @@ class TrainingConfig:
     output_dir: Path
 
 
-class CustomTrainingArguments(TrainingArguments):
-    def __init__(self, config: TrainingConfig):
-        super().__init__(
-            output_dir=str(config.output_dir),
-            learning_rate=2e-5,
-            per_device_train_batch_size=config.batch_size,
-            per_device_eval_batch_size=config.batch_size,
-            num_train_epochs=config.epochs,
-            weight_decay=0.01,
-            evaluation_strategy="epoch",
-            load_best_model_at_end=False,
-            report_to="wandb" if config.wandb_experiment is not None else "none",
-            run_name=config.wandb_experiment,
-        )
+__DEFAULT_ARGS = {
+    "learning_rate": 2e-5,
+    "weight_decay": 1e-2,
+    "evaluation_strategy": "epoch",
+    "load_best_model_at_end": False,
+}
+
+
+def make_training_args(
+    config: TrainingConfig, seq2seq: bool = False
+) -> TrainingArguments:
+    common_args = dict(__DEFAULT_ARGS)
+    common_args["output_dir"] = config.output_dir
+    common_args["per_device_train_batch_size"] = config.batch_size
+    common_args["per_device_eval_batch_size"] = config.batch_size
+    common_args["num_train_epochs"] = config.epochs
+    common_args["report_to"] = (
+        "wandb" if config.wandb_experiment is not None else "none"
+    )
+    common_args["run_name"] = config.wandb_experiment
+
+    if seq2seq:
+        return Seq2SeqTrainingArguments(predict_with_generate=True, **common_args)
+
+    return TrainingArguments(**common_args)
--- dizertatie/training/train.py
+++ dizertatie/training/train.py
@@ -1,21 +1,30 @@
-from typing import Dict
+from typing import Dict, Union
 
 from datasets import Dataset
-from transformers import Trainer
+from transformers import (
+    Seq2SeqTrainer,
+    Seq2SeqTrainingArguments,
+    Trainer,
+    TrainingArguments,
+)
 
 from dizertatie.model.base import BaseModel
-from dizertatie.training.args import CustomTrainingArguments
 from dizertatie.training.metrics import Metrics
 
 
 def train_and_evaluate(
     model: BaseModel,
-    args: CustomTrainingArguments,
+    args: Union[TrainingArguments, Seq2SeqTrainingArguments],
     metrics: Metrics,
     train_set: Dataset,
     test_set: Dataset,
 ) -> Dict[str, float]:
-    trainer = Trainer(
+    trainer_class = Trainer
+
+    if isinstance(args, Seq2SeqTrainingArguments):
+        trainer_class = Seq2SeqTrainer
+
+    trainer = trainer_class(
         model=model.model,
         args=args,
         train_dataset=train_set,
