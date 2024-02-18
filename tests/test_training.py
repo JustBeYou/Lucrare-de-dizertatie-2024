8e30bbe970ffdd8e2761c063b1b2659d9f6df64b
Mihail Feraru <mihailferaru2000@gmail.com>
2024-02-18 14:09:01 +0200

    feature: Training for summarization.

--- tests/test_training.py
+++ tests/test_training.py
@@ -1,13 +1,17 @@
 import unittest
 
+from datasets import Dataset
+
 from dizertatie.configs import DATASET_CONFIG_TESTS, TRAINING_CONFIG_TESTS
 from dizertatie.dataset import load
+from dizertatie.model.base import BaseModel
+from dizertatie.model.robart_seq2seq import RoBartSeq2Seq, RoBartSeq2SeqConfig
 from dizertatie.model.robert_classification import (
     RoBertClassifier,
     RoBertClassifierConfig,
 )
-from dizertatie.training.args import CustomTrainingArguments
-from dizertatie.training.metrics import ClassificationMetrics
+from dizertatie.training.args import make_training_args
+from dizertatie.training.metrics import ClassificationMetrics, SummarizationMetrics
 from dizertatie.training.train import train_and_evaluate
 
 
@@ -18,7 +22,37 @@ class TrainingTestCase(unittest.TestCase):
             RoBertClassifierConfig(num_labels=dataset.features["target"].num_classes)
         )
 
-        tokenized_dataset = (
+        tokenized_dataset = self.__tokenize_dataset(dataset, model)
+
+        results = train_and_evaluate(
+            model,
+            make_training_args(TRAINING_CONFIG_TESTS),
+            ClassificationMetrics(),
+            tokenized_dataset.select(range(0, 5)),
+            tokenized_dataset.select(range(5, 10)),
+        )
+
+        self.assertTrue("eval_loss" in results)
+
+    def test_training_summarization(self):
+        dataset = load(DATASET_CONFIG_TESTS, "RoTextSummarization")
+        model = RoBartSeq2Seq(RoBartSeq2SeqConfig())
+
+        tokenized_dataset = self.__tokenize_dataset(dataset, model)
+
+        results = train_and_evaluate(
+            model,
+            make_training_args(TRAINING_CONFIG_TESTS, seq2seq=True),
+            SummarizationMetrics(model.tokenizer),
+            tokenized_dataset.select(range(0, 5)),
+            tokenized_dataset.select(range(5, 10)),
+        )
+
+        self.assertTrue("eval_loss" in results)
+
+    @staticmethod
+    def __tokenize_dataset(dataset: Dataset, model: BaseModel) -> Dataset:
+        return (
             dataset.map(
                 lambda x: model.tokenize(x, 16),
                 remove_columns=list(
@@ -32,16 +66,6 @@ class TrainingTestCase(unittest.TestCase):
             .rename_column("target", "labels")
             .with_format("torch")
         )
-
-        results = train_and_evaluate(
-            model,
-            CustomTrainingArguments(TRAINING_CONFIG_TESTS),
-            ClassificationMetrics(),
-            tokenized_dataset.select(range(0, 5)),
-            tokenized_dataset.select(range(5, 10)),
-        )
-
-        self.assertTrue("eval_loss" in results)
 
 
 if __name__ == "__main__":
